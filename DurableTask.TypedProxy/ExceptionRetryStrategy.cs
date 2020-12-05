7d3e21dcf036f52f75964dab1ec6539a7605a60b
Tatsuro Shibamura <me@shibayan.jp>
2020-12-05 20:50:38 +0900

    Adding exception retry strategy (#14)

---
+++ DurableTask.TypedProxy/ExceptionRetryStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DurableTask.TypedProxy
+{
+    public static class ExceptionRetryStrategy<TException>
+    {
+        public static bool Handle(Exception exception)
+        {
+            return exception.InnerException is TException;
+        }
+    }
+}
